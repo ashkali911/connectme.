9c033aac8e1958cbb5ebbcb12ffcce6468778a79
ashkali911 <135557589+ashkali911@users.noreply.github.com>
2023-06-05 01:11:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,34 @@
-// Sample profile data for demonstration
-var profile = {
-    fullName: "John Doe",
-    username: "johndoe",
-    email: "johndoe@example.com",
-    picture: "profile-picture.jpg",
-    followers: 150
-};
+// Sign Up Function
+function signUp() {
+    var username = document.getElementById("signup-username").value;
+    var password = document.getElementById("signup-password").value;
 
-// Update the profile section with data
-function updateProfile() {
-    document.getElementById("full-name").textContent = profile.fullName;
-    document.getElementById("username").textContent = "@" + profile.username;
-    document.getElementById("email").textContent = "Email: " + profile.email;
-    document.getElementById("profile-picture").src = profile.picture;
-    document.getElementById("follower-count").textContent = profile.followers;
-}
-
-// Edit profile
-function editProfile() {
-    var newFullName = prompt("Enter your full name", profile.fullName);
-    var newUsername = prompt("Enter your username", profile.username);
-    var newEmail = prompt("Enter your email", profile.email);
-
-    if (newFullName && newUsername && newEmail) {
-        profile.fullName = newFullName;
-        profile.username = newUsername;
-        profile.email = newEmail;
-        updateProfile();
+    if (username && password) {
+        localStorage.setItem(username, password);
+        alert("Account created successfully!");
+    } else {
+        alert("Please enter both username and password.");
     }
 }
 
-document.getElementById("edit-profile-button").addEventListener("click", editProfile);
+document.getElementById("signup-button").addEventListener("click", signUp);
 
-// Sample data for demonstration
-var posts = [
-    { id: 1, text: "Hello, ConnectMe!" },
-    { id: 2, text: "It's a great day!" }
-];
+// Login Function
+function login() {
+    var username = document.getElementById("login-username").value;
+    var password = document.getElementById("login-password").value;
 
-function displayPosts() {
-    var postList = document.getElementById("post-list");
-    postList.innerHTML = "";
-
-    posts.forEach(function(post) {
-        var postElement = document.createElement("div");
-        postElement.classList.add("post");
-        postElement.innerHTML = post.text;
-        postList.appendChild(postElement);
-    });
+    if (username && password) {
+        if (localStorage.getItem(username) === password) {
+            alert("Login successful!");
+            // You can redirect the user to a new page after successful login
+            // window.location.href = "/homepage.html";
+        } else {
+            alert("Invalid username or password.");
+        }
+    } else {
+        alert("Please enter both username and password.");
+    }
 }
 
-function addPost() {
-    var postText = document.getElementById("post-text").value;
-    var postId = posts.length + 1;
-    var newPost = { id: postId, text: postText };
-    posts.push(newPost);
-    displayPosts();
-    document.getElementById("post-text").value = "";
-}
-
-document.getElementById("post-button").addEventListener("click", addPost);
-
-// Initial display of profile and posts
-updateProfile();
-displayPosts();
- 
+document.getElementById("login-button").addEventListener("click", login);
